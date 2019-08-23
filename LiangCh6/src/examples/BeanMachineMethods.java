6ef07c5ad8e45969288d0e216f83d1962694a87f
Instructor <Instructor>
2019-08-23 14:23:12 -0500

    added more starting notes.

--- LiangCh6/src/examples/BeanMachineMethods.java
+++ LiangCh6/src/examples/BeanMachineMethods.java
@@ -4,7 +4,30 @@ public class BeanMachineMethods {
 
 	public static void main(String[] args) {
 		System.out.println("Hello.");
-
+		
+		// 1. array for the plinco chips to fall into.
+		// Hamza and Qusai
+		// branch: winningCups
+		
+		// 2. a way to find out what the cup the chip fell into
+		// Henry and Cyrus
+		// branch: path
+		
+		// 3. a way to print results
+		// Mahdi and Andrew
+		// output
+		
+		// 4. ask user where to put the chip in the beginning.
+		// Khalid and Jeremy
+		// branch: userStartLocation
+		
+		// 5. keep track of winnings
+		// Dr. Ghosh and Malachi
+		// scoreboard
+		
+		// 6. do multiple chips.
+		// Hussain and Anthony
+		// multiChip
+		
 	}
-
 }
