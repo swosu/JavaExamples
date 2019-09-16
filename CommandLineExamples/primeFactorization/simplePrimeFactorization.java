e090c2e1b76920ae8b0631050bfa085daa36ecac
Jeremy Evert <jeremy.evert@swosu.edu>
2019-09-15 21:55:11 -0500

    added custom method.

--- CommandLineExamples/primeFactorization/simplePrimeFactorization.java
+++ CommandLineExamples/primeFactorization/simplePrimeFactorization.java
@@ -5,14 +5,24 @@ public class simplePrimeFactorization {
         System.out.println("Hello, World");
 
         int firstPrime = 8;
+        int remainder = firstPrime;
+        // 8 is 2 * 2 * 2.
+        // so the sum of the factors is 6.
         boolean remainderIsPrime = false;
 
         while(!remainderIsPrime)
         {
+          remainderIsPrime = checkRemainder(remainder);
           System.out.println("Hello inside of loop.");
-          remainderIsPrime = true;
+
         }
         System.out.println("Thank you and good night.");
     }
+
+    private static boolean checkRemainder(int remainder){
+       System.out.println("Hello inside method.");
+       boolean remainderIsPrime = true;
+       return remainderIsPrime;
+   }
 
 }
