245cebfc010aea39433b224beec06ef33fc67716
Jeremy Evert <jeremy.evert@swosu.edu>
2019-09-15 22:17:37 -0500

    have a really bad code for checking for primes.

--- CommandLineExamples/primeFactorization/simplePrimeFactorization.java
+++ CommandLineExamples/primeFactorization/simplePrimeFactorization.java
@@ -1,28 +1,42 @@
 public class simplePrimeFactorization {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+  public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
 
-        int firstPrime = 8;
-        int remainder = firstPrime;
-        // 8 is 2 * 2 * 2.
-        // so the sum of the factors is 6.
-        boolean remainderIsPrime = false;
+    int firstPrime = 8;
+    System.out.printf("our first prime is %d.\n", firstPrime);
+    int remainder = firstPrime;
+    System.out.printf("our initial remainder is %d.\n", remainder);
+    // 8 is 2 * 2 * 2.
+    // so the sum of the factors is 6.
 
-        while(!remainderIsPrime)
-        {
-          remainderIsPrime = checkRemainder(remainder);
-          System.out.println("Hello inside of loop.");
-
-        }
-        System.out.println("Thank you and good night.");
+    System.out.println("Start of algorithm.");
+    // is the remainder prime? if so, we quit.
+    boolean remainderIsPrime = checkRemainder(remainder);
+    if(remainderIsPrime)
+    {
+        System.out.printf("remainder is prime.\n");
     }
+    else
+    {
+      System.out.printf("remainder is not prime.\n");
+    }
+    
+    System.out.println("Thank you and good night.");
+  }
 
-    private static boolean checkRemainder(int remainder){
-       System.out.println("Hello inside method.");
-       boolean remainderIsPrime = true;
-       return remainderIsPrime;
-   }
+  private static boolean checkRemainder(int remainder){
+    System.out.println("Hello inside method.");
+    boolean remainderIsPrime = true;
+    for(int checkValue = 2; checkValue < remainder; checkValue++)
+    {
+      if(0 == (remainder%checkValue))
+      {
+        remainderIsPrime = false;
+      }
+    }
+    return remainderIsPrime;
+  }
 
 }
