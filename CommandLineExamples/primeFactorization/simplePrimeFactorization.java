c419efb03adced531274e662b9ebe53afd835f1c
Jeremy Evert <jeremy.evert@swosu.edu>
2019-09-15 21:48:48 -0500

    starting with the project.

---
+++ CommandLineExamples/primeFactorization/simplePrimeFactorization.java
@@ -0,0 +1,18 @@
+public class simplePrimeFactorization {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+
+        int firstPrime = 8;
+        boolean remainderIsPrime = false;
+
+        while(!remainderIsPrime)
+        {
+          System.out.println("Hello inside of loop.");
+          remainderIsPrime = true;
+        }
+        System.out.println("Thank you and good night.");
+    }
+
+}
