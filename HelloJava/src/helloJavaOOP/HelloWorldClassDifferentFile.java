6a3f9cb2157cabcc642f2bf150f7e9c86a661a5e
Jeremy Evert J8VM <jeremy.evert@swosu.edu>
2020-03-16 13:56:00 -0500

    added OOP example.

---
+++ HelloJava/src/helloJavaOOP/HelloWorldClassDifferentFile.java
@@ -0,0 +1,8 @@
+package helloJavaOOP;
+public class HelloWorldClassDifferentFile {
+	public static void main(String[] args) {
+		customClassHelloJavaClassDifferentFile ourOtherObject = 
+				new customClassHelloJavaClassDifferentFile();
+		ourOtherObject.sayHello();
+	}
+}
---
+++ HelloJava/src/helloJavaOOP/customClassHelloJavaClassDifferentFile.java
@@ -0,0 +1,6 @@
+package helloJavaOOP;
+public class customClassHelloJavaClassDifferentFile {
+	public void sayHello() {
+		System.out.println("Hello Java from inside a class in a different file!");
+	}
+}
