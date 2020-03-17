f577187e5d60cf6a0248437aa189d5050f76deed
Jeremy Evert J8VM <jeremy.evert@swosu.edu>
2020-03-17 14:04:54 -0500

    added a custom method example.:

---
+++ HelloOOP/src/simpleOOP/HelloWorldCustomMethod.java
@@ -0,0 +1,15 @@
+package simpleOOP;
+
+public class HelloWorldCustomMethod {
+
+	public static void main(String[] args) {
+		helloFromCustomMethod();
+
+	}
+
+	private static void helloFromCustomMethod() {
+		System.out.println("Hello, World, from inside a custom method.");
+		
+	}
+	
+}
