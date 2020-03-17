f9e3deddcf66b3754cf2bb44267e916c156c7f01
Jeremy Evert J8VM <jeremy.evert@swosu.edu>
2020-03-17 14:17:30 -0500

    added an oop example with two files.

---
+++ HelloOOP/src/simpleOOP/HelloClassDefinitionSeperateFile.java
@@ -0,0 +1,13 @@
+package simpleOOP;
+
+public class HelloClassDefinitionSeperateFile {
+	HelloClassDefinitionSeperateFile(){
+		
+	}
+
+	public void sayHelloFromClassDefinitionInADifferentFile() {
+		System.out.println("Hello from a different class in a different file.");
+		
+	}
+
+}
---
+++ HelloOOP/src/simpleOOP/HelloCustomClassFirstFile.java
@@ -0,0 +1,11 @@
+package simpleOOP;
+
+public class HelloCustomClassFirstFile {
+
+	public static void main(String[] args) {
+		HelloClassDefinitionSeperateFile ourOtherObject = new HelloClassDefinitionSeperateFile();
+		ourOtherObject.sayHelloFromClassDefinitionInADifferentFile();
+
+	}
+
+}
