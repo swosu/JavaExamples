0bdd0894b1d76528c1945831797868a53d2b7049
Jeremy Evert J8VM <jeremy.evert@swosu.edu>
2020-03-21 16:26:18 -0500

    deleted broken or less useful files.

--- LiangCh1/src/Listings/Ch1Listing6.java
+++
@@ -1,8 +0,0 @@
-package Listings;
-
-public class Ch1Listing6 {
-	public static void main(String[] args) {
-		System.out.println("Celsius 35 is Fahrenheit degree ");
-		System.out.println((9 / 5) * 35 + 32);
-	}
-}
