410eef761d9f9b121b95501aa9735539ad4073dc
Jeremy Evert J8VM <jeremy.evert@swosu.edu>
2020-04-03 15:20:17 -0500

    added a couple more Java Examples.

--- DiscreteStructures/src/ch10ProgrammingPracticum/OurParty.java
+++ DiscreteStructures/src/ch10ProgrammingPracticum/OurParty.java
@@ -2,32 +2,90 @@ package ch10ProgrammingPracticum;
 
 public class OurParty {
 
+	private int handshakeSum;
 	private int partyGoerCount;
 	private double oddsToShake;
 	private double oddsToWantToTry;
-	private double rejectionPenalty;
-	private double repeatPenalty;
 
 	public OurParty(
 			int constructionPartyGoers, 
 			double constructionOddsToShake, 
-			double constructionOddsToTry, 
-			double constructionRejectionPenalty,
-			double constructionRepeatPenalty) {
+			double constructionOddsToTry) {
 		this.partyGoerCount = constructionPartyGoers;
 		this.oddsToShake = constructionOddsToShake;
 		this.oddsToWantToTry = constructionOddsToTry;
-		this.rejectionPenalty = constructionRejectionPenalty;
-		this.repeatPenalty = constructionRepeatPenalty;
 	}
 
 	public int getPartySize() {
 		return this.partyGoerCount;
 	}
 
-	public void mixer(boolean[][] shakeMatrix) {
-		// TODO Auto-generated method stub
-		
+	public void mixer(int[][] shakeMatrix) {
+		int tempHandshakeTracker;
+		for(int individualIndex = 0; individualIndex < shakeMatrix.length; individualIndex++)
+		{
+			for(int partnerIndex = 0; partnerIndex < shakeMatrix.length; partnerIndex++)
+			{
+				if(individualIndex==partnerIndex)
+				{
+					tempHandshakeTracker = 0; // don't shake your own hand.
+				} 
+				else 
+				{
+					tempHandshakeTracker = handShakeAttempt();
+				}
+
+				shakeMatrix[individualIndex][partnerIndex] += tempHandshakeTracker;
+				// cause if you shake my hand, I also shake yours...
+				shakeMatrix[partnerIndex][individualIndex] += tempHandshakeTracker;
+			}
+		}
 	}
 
+	private int handShakeAttempt() {
+		if(Math.random() < this.oddsToWantToTry )
+		{
+			// we attempt a shake
+			if(Math.random() < this.oddsToShake)
+			{
+				// we had a successful shake
+				return 1;
+			}
+		}
+		return 0; // either no shake was attempted or the other person did not shake back.
+	}
+
+	public void printMatrix(int[][] shakeMatrix) {
+		System.out.println("This matrix should be symmetric.");
+		for(int individualIndex = 0; individualIndex < shakeMatrix.length; individualIndex++)
+		{
+			for(int partnerIndex = 0; partnerIndex < shakeMatrix.length; partnerIndex++)
+			{
+				System.out.print(shakeMatrix[individualIndex][partnerIndex] + ", ");
+			}
+			System.out.println();
+		}
+	}
+
+	public void countHandshakes(int[][] shakeMatrix) {
+		for(int individualIndex = 0; individualIndex < shakeMatrix.length; individualIndex++)
+		{
+			for(int partnerIndex = 0; partnerIndex < shakeMatrix.length; partnerIndex++)
+			{
+				this.handshakeSum += shakeMatrix[individualIndex][partnerIndex];
+			}
+		}
+	}
+
+	public void printHandshaekCount() {
+		System.out.println("Our handshake sum was: " + this.handshakeSum);
+		if(0==this.handshakeSum%2) 
+		{
+			System.out.println("This was an even count");
+		} 
+		else
+		{
+			System.out.println("We did not have an even count.");
+		}
+	}
 }
--- DiscreteStructures/src/ch10ProgrammingPracticum/countingHandshakes.java
+++ DiscreteStructures/src/ch10ProgrammingPracticum/countingHandshakes.java
@@ -3,15 +3,30 @@ package ch10ProgrammingPracticum;
 public class countingHandshakes {
 
 	public static void main(String[] args) {
-
+		System.out.println("Hello");
 		
-		OurParty tightParty = new OurParty(5,0.8,0.7,0.05,0.1);
+		OurParty tightParty = new OurParty(5,0.8,0.7);
+		int[][] tightPartyShakeMatrix = 
+				new int [tightParty.getPartySize()][tightParty.getPartySize()];
 		
-		boolean[][] shakeMatrix = 
-				new boolean [tightParty.getPartySize()][tightParty.getPartySize()];
 		
-		tightParty.mixer(shakeMatrix);
-
+		tightParty.mixer(tightPartyShakeMatrix);
+		
+		tightParty.printMatrix(tightPartyShakeMatrix);
+		tightParty.countHandshakes(tightPartyShakeMatrix);
+		tightParty.printHandshaekCount();
+		
+		OurParty corperateParty = new OurParty(50,0.08,0.07);
+		int[][] corperatePartyShakeMatrix = 
+				new int [corperateParty.getPartySize()][corperateParty.getPartySize()];
+		
+		
+		corperateParty.mixer(corperatePartyShakeMatrix);
+		
+		corperateParty.printMatrix(corperatePartyShakeMatrix);
+		corperateParty.countHandshakes(corperatePartyShakeMatrix);
+		corperateParty.printHandshaekCount();
+		System.out.println("Bye.");
 	}
 
 }
