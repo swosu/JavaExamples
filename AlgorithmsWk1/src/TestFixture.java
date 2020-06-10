d9747e74f6271e1fdac171b4b8fd1ffd65a5db7e
Jeremy Evert Maise <jeremy.evert@swosu.edu>
2020-06-10 17:01:28 -0500

    added more to the java examples working towards lists.

--- AlgorithmsWk1/src/TestFixture.java
+++ AlgorithmsWk1/src/TestFixture.java
@@ -1,11 +1,10 @@
+import java.util.LinkedList;
 
 public class TestFixture {
-	
 	private int arraySize;
 
 	public void setArraySize(int arraySize) {
 		this.arraySize= arraySize;
-		
 	}
 
 	public int getArraySize() {
@@ -15,8 +14,116 @@ public class TestFixture {
 	public void fillArray(int[] startingArray) {
 		for(int index = 0; index < startingArray.length; index++)
 		{
-			startingArray[index] = (int)(Math.random()*2000);
+			startingArray[index] = (int)(Math.random()*2000 * startingArray.length);
 		}
 	}
 
+	public void printArray(int[] array) {
+		for(int index : array)
+		{
+			System.out.print(index + ", ");
+			if(0 == index % 10)
+			{
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+
+	public void copyArray(int[] startingArray, int[] mergeSortArrayCopy) {
+		for(int index = 0; index < startingArray.length; index++)
+		{
+			mergeSortArrayCopy[index] = startingArray[index];
+		}
+	}
+
+	public void mergeSortTheArray(int[] list) {
+		if (list.length > 1) {
+			// Merge sort the first half
+			int[] firstHalf = new int[list.length / 2];
+			System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+			mergeSortTheArray(firstHalf);
+			// Merge sort the second half
+			int secondHalfLength = list.length - list.length / 2;
+			int[] secondHalf = new int[secondHalfLength];
+			System.arraycopy(list, list.length / 2,
+					secondHalf, 0, secondHalfLength);
+			mergeSortTheArray(secondHalf);
+			// Merge firstHalf with secondHalf into list
+			merge(firstHalf, secondHalf, list);
+		}
+	}
+
+	/** Merge two sorted lists */
+	private void merge(int[] list1, int[] list2, int[] temp) {
+
+		int current1 = 0; 
+		int current2 = 0; 
+		int current3 = 0; 
+
+		while (current1 < list1.length && current2 < list2.length) {
+			if (list1[current1] < list2[current2])
+				temp[current3++] = list1[current1++];
+			else
+				temp[current3++] = list2[current2++];
+		}
+		while (current1 < list1.length)
+			temp[current3++] = list1[current1++];
+		while (current2 < list2.length)
+			temp[current3++] = list2[current2++];
+	}
+
+	public void quickSort(int[] list) {
+		quickSort(list, 0, list.length - 1);
+	}
+
+	public static void quickSort(int[] list, int first, int last) {
+		if (last > first) {
+			int pivotIndex = partition(list, first, last);
+			quickSort(list, first, pivotIndex - 1);
+			quickSort(list, pivotIndex + 1, last);
+		}
+	}
+
+	/** Partition the array list[first..last] */
+	private static int partition(int[] list, int first, int last) {
+		int pivot = list[first]; // Choose the first element as the pivot
+		int low = first + 1; // Index for forward search
+		int high = last; // Index for backward search
+		while (high > low) {
+			// Search forward from left
+			while (low <= high && list[low] <= pivot)
+				low++;
+			// Search backward from right
+			while (low <= high && list[high] > pivot)
+				high--;
+			// Swap two elements in the list
+			if (high > low) {
+				int temp = list[high];
+				list[high] = list[low];
+				list[low] = temp;
+			}
+		}
+		while (high > first && list[high] >= pivot)
+			high--;
+		// Swap pivot with list[high]
+		if (pivot > list[high]) {
+			list[first] = list[high];
+			list[high] = pivot;
+			return high;
+		}
+		else {
+			return first;
+		}
+	}
+
+	public void copyStartingArrayToStartingList(
+			int[] startingArray, 
+			LinkedList<Integer> startingList) 
+	{
+		for(int index = 0; index < startingArray.length; index++)
+		{
+			startingList.addLast(startingArray[index]);
+		}
+	}
 }
--- AlgorithmsWk1/src/Week1FixtureDivideAndConquerSorting.java
+++ AlgorithmsWk1/src/Week1FixtureDivideAndConquerSorting.java
@@ -1,9 +1,7 @@
+import java.util.LinkedList;
 
 public class Week1FixtureDivideAndConquerSorting {
-
 	public static void main(String[] args) {
-		System.out.println("Hello.");
-		
 		/*Divide and Conquer can be useful for many problems. 
 		 * One common application is sorting, 
 		 * which takes place frequently in programming. 
@@ -22,19 +20,46 @@ public class Week1FixtureDivideAndConquerSorting {
 		 * Please use an input set large enough to provide data 
 		 * suggesting a clear winner. 
 		 */
-		
+
 		TestFixture fixture = new TestFixture();
-		fixture.setArraySize(5);
-		
-		// TODO Make array to sort
-		int[] startingArray = new int[fixture.getArraySize()];
-		fixture.fillArray(startingArray);
-		fixture.printArray(startingArray);
-		
-		// TODO Make a copy for Merge Sort
-		
-		// TODO Make a copy for Quick Sort
+		System.out.println("Time in milliseconds");
+		System.out.println("Array size, \t merge sort, \t quick sort");
+		for(int repeatCount = 1; repeatCount < 3; repeatCount++)
+		{
+			fixture.setArraySize(50);
+			System.out.print(fixture.getArraySize() + "\t");
 
+			// Make array to sort
+			int[] startingArray = new int[fixture.getArraySize()];
+			fixture.fillArray(startingArray);
+			//System.out.println("Starting Array");
+			//fixture.printArray(startingArray);
+			LinkedList<Integer> startingList=new LinkedList<Integer>();
+			fixture.copyStartingArrayToStartingList(startingArray,startingList);
+
+			// Make a copy for Merge Sort
+			int[] mergeSortArrayCopy = new int[fixture.getArraySize()];
+			fixture.copyArray(startingArray, mergeSortArrayCopy);
+			//fixture.printArray(mergeSortArrayCopy);
+
+			// Make a copy for Quick Sort
+			int[] quickSortArrayCopy = new int[fixture.getArraySize()];
+			fixture.copyArray(startingArray, quickSortArrayCopy);
+			//fixture.printArray(quickSortArrayCopy);
+
+			// sort with merge sort
+			long startTime = System.currentTimeMillis();
+			fixture.mergeSortTheArray(mergeSortArrayCopy);
+			long endTime = System.currentTimeMillis();
+			System.out.print((endTime - startTime) + "\t");
+			//fixture.printArray(mergeSortArrayCopy);
+
+			// sort with quick sort
+			startTime = System.currentTimeMillis();
+			fixture.quickSort(quickSortArrayCopy);
+			endTime = System.currentTimeMillis();
+			System.out.println((endTime - startTime) + "\t");
+			//fixture.printArray(quickSortArrayCopy);
+		}
 	}
-
 }
